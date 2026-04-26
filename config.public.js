ae27f5e2ebe49c8ed14db93bcae7827e3f7357b3
cristianvargasippc-art <cristianvargasippc@gmail.com>
2026-04-26 15:58:57 -0400

    commint

---
+++ config.public.js
@@ -0,0 +1,9 @@
+window.__SIGEL_FIREBASE_CONFIG__ = {
+  apiKey: 'AIzaSyDNAoUx40Ax8cu0IKREgle3Bnf271NdKXo',
+  authDomain: 'sigel-celider-10-04.firebaseapp.com',
+  projectId: 'sigel-celider-10-04',
+  storageBucket: 'sigel-celider-10-04.firebasestorage.app',
+  messagingSenderId: '331346792233',
+  appId: '1:331346792233:web:15589aa0b9b8c522d2f138',
+  databaseURL: 'https://sigel-celider-10-04-default-rtdb.firebaseio.com'
+};
